54ba42ad766494447eae922d986087215c575c27
Teng Akala Li <akalalee86@gmail.com>
2016-10-16 18:44:08 +0200

    Initial commit for the dice game

---
+++ dice_game.py
@@ -0,0 +1,18 @@
+from random import randint as dice
+
+secret_number= dice(1,9)
+name = input("Welcome to the dice game. What is your name? ")
+guess_number = 0
+loopcount = 5
+while(guess_number!=secret_number and loopcount >=1):
+	guess_number=input("Hello " + name+" please pick a interge number between 1 and 9 :")
+	guess_number = int(guess_number)
+	loopcount=(loopcount)-1
+	if (guess_number == secret_number and loopcount>0):
+		print ("Your number is correct. You won the game")
+	elif(guess_number>secret_number and loopcount>0):
+			print ("Try a lower number.Your remaining guess is "+str(loopcount))
+	elif (guess_number<secret_number and loopcount>0):
+			print ("Try a higher number.Your remaining guess is "+str(loopcount))
+	
+
